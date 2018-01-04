274462ca4c2dfe9646da912195972f70d3256a04
Anonymous <timbinquim@yahoo.com.br>
2018-01-03 21:17:40 -0300

    Start

---
+++ Assets/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Stats
+{
+    [Range(0.0f, 1.0f)]
+    public float body;
+    [Range(0.0f, 1.0f)]
+    public float smart;
+    [Range(0.0f, 1.0f)]
+    public float genki;
+    [Range(0.0f, 1.0f)]
+    public float cute;
+}
+
+[CreateAssetMenu(fileName = "Data", menuName = "Enum/Persona", order = 1)]
+public class Persona : ScriptableObject 
+{
+    public string firstName;
+    public string secondName;
+    public Stats stats;
+    public Stats exigence;
+}
---
+++ Assets/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Não sei se vai ter uso isso.
+
+public enum Girls { Tomoko, Mayuri, Minori, Asuka, Saya, Aoba, Karen, Misato };
+public enum Boys { Araragi, Hachiman, Armin, Kitamura, Okabe, Nagito, Byakuya, Kaiki };
+
+public class Tests : MonoBehaviour 
+{
+    public Persona p;
+
+    void Start()
+    {
+        Debug.Log(p.ToString());
+    }
+}
